e031db8002fcf5744bdde04b6d7adb92806bd51c
Avinash kumar pandey <105911954+avinash-kr-pandey@users.noreply.github.com>
2025-10-22 19:33:40 +0530

    new code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,11 @@
+import AboutHeader from '@/components/About/AboutHeader'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-gray-900'>About</div>
+    <div className='text-gray-900'>
+      <AboutHeader />
+    </div>
   )
 }
 
